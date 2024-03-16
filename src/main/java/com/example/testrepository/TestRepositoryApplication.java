e1a27d3ba8caba1dc59a76a3063bc514d315d51c
BuchnevDmitry <buchnev03.03@mail.ru>
2024-03-17 01:45:07 +0300

    Second commit

--- src/main/java/com/example/testrepository/TestRepositoryApplication.java
+++ src/main/java/com/example/testrepository/TestRepositoryApplication.java
@@ -8,7 +8,7 @@ public class TestRepositoryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestRepositoryApplication.class, args);
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world123!");
     }
 
 }
